661940eb6ccfe87fcb8219dd198cf058c3c2fdd7
Memoona-tech <156640976+Memoona-tech@users.noreply.github.com>
2026-04-16 16:05:32 +0500

    Update resume link to point to Memoona-Resume.pdf

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -54,7 +54,7 @@ const Sidebar: React.FC<Props> = ({ isOpen, onClose, pathName }: Props) => {
         ))}
 
         <Link
-            href="resume.pdf"
+            href="Memoona-Resume.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="text-sm bg-lightSky/10 px-4 py-2 rounded-md border border-hoverColor/10 hover:border-hoverColor hover:bg-hoverColor hover:text-black hoverEffect"
