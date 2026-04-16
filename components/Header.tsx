aee973681a421a8c11fb7b92ad7cb0f4a3975100
Memoona-tech <156640976+Memoona-tech@users.noreply.github.com>
2026-04-16 16:56:33 +0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -47,7 +47,7 @@ const Header = () => {
           ))}
 
           <Link
-            href="resume.pdf"
+            href="Memoona-Resume.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="text-sm bg-lightSky/10 px-4 py-2 rounded-md border border-hoverColor/10 hover:border-hoverColor hover:bg-hoverColor hover:text-black hoverEffect"
